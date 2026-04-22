cbf53e3bd5cb637a0f4f98e02124427000e5f35d
kezyakaonang106-rgb <kezyakaonang106@student.unsrat.ac.id>
2026-04-22 16:39:05 +0800

    Add files via upload

---
+++ Kezya/qgis2web_2026_04_21-20_29_31_010199/layers/layers.js
@@ -0,0 +1,73 @@
+var wms_layers = [];
+
+
+        var lyr_GoogleSatellite_0 = new ol.layer.Tile({
+            'title': 'Google Satellite',
+            'opacity': 1.000000,
+            
+            
+            source: new ol.source.XYZ({
+            attributions: '&nbsp;&middot; <a href="https://www.google.at/permissions/geoguidelines/attr-guide.html">Map data ©2015 Google</a>',
+                url: 'https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}'
+            })
+        });
+var format_KOTAMOBAGUUTARA_1 = new ol.format.GeoJSON();
+var features_KOTAMOBAGUUTARA_1 = format_KOTAMOBAGUUTARA_1.readFeatures(json_KOTAMOBAGUUTARA_1, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_KOTAMOBAGUUTARA_1 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_KOTAMOBAGUUTARA_1.addFeatures(features_KOTAMOBAGUUTARA_1);
+var lyr_KOTAMOBAGUUTARA_1 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_KOTAMOBAGUUTARA_1, 
+                style: style_KOTAMOBAGUUTARA_1,
+                popuplayertitle: 'KOTAMOBAGU UTARA',
+                interactive: true,
+                title: '<img src="styles/legend/KOTAMOBAGUUTARA_1.png" /> KOTAMOBAGU UTARA'
+            });
+var format_KelurahanBiga_2 = new ol.format.GeoJSON();
+var features_KelurahanBiga_2 = format_KelurahanBiga_2.readFeatures(json_KelurahanBiga_2, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_KelurahanBiga_2 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_KelurahanBiga_2.addFeatures(features_KelurahanBiga_2);
+var lyr_KelurahanBiga_2 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_KelurahanBiga_2, 
+                style: style_KelurahanBiga_2,
+                popuplayertitle: 'Kelurahan Biga',
+                interactive: true,
+                title: '<img src="styles/legend/KelurahanBiga_2.png" /> Kelurahan Biga'
+            });
+var format_PEMUKIMAN_3 = new ol.format.GeoJSON();
+var features_PEMUKIMAN_3 = format_PEMUKIMAN_3.readFeatures(json_PEMUKIMAN_3, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_PEMUKIMAN_3 = new ol.source.Vector({
+    attributions: ' ',
+});
+jsonSource_PEMUKIMAN_3.addFeatures(features_PEMUKIMAN_3);
+var lyr_PEMUKIMAN_3 = new ol.layer.Vector({
+                declutter: false,
+                source:jsonSource_PEMUKIMAN_3, 
+                style: style_PEMUKIMAN_3,
+                popuplayertitle: 'PEMUKIMAN',
+                interactive: true,
+                title: '<img src="styles/legend/PEMUKIMAN_3.png" /> PEMUKIMAN'
+            });
+
+lyr_GoogleSatellite_0.setVisible(true);lyr_KOTAMOBAGUUTARA_1.setVisible(true);lyr_KelurahanBiga_2.setVisible(true);lyr_PEMUKIMAN_3.setVisible(true);
+var layersList = [lyr_GoogleSatellite_0,lyr_KOTAMOBAGUUTARA_1,lyr_KelurahanBiga_2,lyr_PEMUKIMAN_3];
+lyr_KOTAMOBAGUUTARA_1.set('fieldAliases', {'KDPPUM': 'KDPPUM', 'NAMOBJ': 'NAMOBJ', 'REMARK': 'REMARK', 'KDPBPS': 'KDPBPS', 'FCODE': 'FCODE', 'LUASWH': 'LUASWH', 'UUPP': 'UUPP', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDBBPS': 'KDBBPS', 'KDBPUM': 'KDBPUM', 'WADMKD': 'WADMKD', 'WIADKD': 'WIADKD', 'WADMKC': 'WADMKC', 'WIADKC': 'WIADKC', 'WADMKK': 'WADMKK', 'WIADKK': 'WIADKK', 'WADMPR': 'WADMPR', 'WIADPR': 'WIADPR', 'TIPADM': 'TIPADM', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_KelurahanBiga_2.set('fieldAliases', {'OBJECTID': 'OBJECTID', 'NAMOBJ': 'NAMOBJ', 'FCODE': 'FCODE', 'REMARK': 'REMARK', 'METADATA': 'METADATA', 'SRS_ID': 'SRS_ID', 'KDBBPS': 'KDBBPS', 'KDCBPS': 'KDCBPS', 'KDCPUM': 'KDCPUM', 'KDEBPS': 'KDEBPS', 'KDEPUM': 'KDEPUM', 'KDPBPS': 'KDPBPS', 'KDPKAB': 'KDPKAB', 'KDPPUM': 'KDPPUM', 'LUASWH': 'LUASWH', 'TIPADM': 'TIPADM', 'WADMKC': 'WADMKC', 'WADMKD': 'WADMKD', 'WADMKK': 'WADMKK', 'WADMPR': 'WADMPR', 'WIADKC': 'WIADKC', 'WIADKK': 'WIADKK', 'WIADPR': 'WIADPR', 'WIADKD': 'WIADKD', 'UUPP': 'UUPP', 'LUAS': 'LUAS', });
+lyr_PEMUKIMAN_3.set('fieldAliases', {'NAMOBJ': 'NAMOBJ', 'FCODE': 'FCODE', 'REMARK': 'REMARK', 'SRS_ID': 'SRS_ID', 'LCODE': 'LCODE', 'METADATA': 'METADATA', 'SHAPE_Leng': 'SHAPE_Leng', 'SHAPE_Area': 'SHAPE_Area', });
+lyr_KOTAMOBAGUUTARA_1.set('fieldImages', {'KDPPUM': 'TextEdit', 'NAMOBJ': 'TextEdit', 'REMARK': 'TextEdit', 'KDPBPS': 'TextEdit', 'FCODE': 'TextEdit', 'LUASWH': 'TextEdit', 'UUPP': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDBPUM': 'TextEdit', 'WADMKD': 'TextEdit', 'WIADKD': 'TextEdit', 'WADMKC': 'TextEdit', 'WIADKC': 'TextEdit', 'WADMKK': 'TextEdit', 'WIADKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADPR': 'TextEdit', 'TIPADM': 'TextEdit', 'SHAPE_Leng': 'TextEdit', 'SHAPE_Area': 'TextEdit', });
+lyr_KelurahanBiga_2.set('fieldImages', {'OBJECTID': 'TextEdit', 'NAMOBJ': 'TextEdit', 'FCODE': 'TextEdit', 'REMARK': 'TextEdit', 'METADATA': 'TextEdit', 'SRS_ID': 'TextEdit', 'KDBBPS': 'TextEdit', 'KDCBPS': 'TextEdit', 'KDCPUM': 'TextEdit', 'KDEBPS': 'TextEdit', 'KDEPUM': 'TextEdit', 'KDPBPS': 'TextEdit', 'KDPKAB': 'TextEdit', 'KDPPUM': 'TextEdit', 'LUASWH': 'TextEdit', 'TIPADM': 'TextEdit', 'WADMKC': 'TextEdit', 'WADMKD': 'TextEdit', 'WADMKK': 'TextEdit', 'WADMPR': 'TextEdit', 'WIADKC': 'TextEdit', 'WIADKK': 'TextEdit', 'WIADPR': 'TextEdit', 'WIADKD': 'TextEdit', 'UUPP': 'TextEdit', 'LUAS': 'TextEdit', });
+lyr_PEMUKIMAN_3.set('fieldImages', {'NAMOBJ': 'TextEdit', 'FCODE': 'TextEdit', 'REMARK': 'TextEdit', 'SRS_ID': 'TextEdit', 'LCODE': 'TextEdit', 'METADATA': 'TextEdit', 'SHAPE_Leng': 'TextEdit', 'SHAPE_Area': 'TextEdit', });
+lyr_KOTAMOBAGUUTARA_1.set('fieldLabels', {'KDPPUM': 'no label', 'NAMOBJ': 'inline label - always visible', 'REMARK': 'no label', 'KDPBPS': 'no label', 'FCODE': 'no label', 'LUASWH': 'no label', 'UUPP': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDBBPS': 'no label', 'KDBPUM': 'no label', 'WADMKD': 'no label', 'WIADKD': 'no label', 'WADMKC': 'no label', 'WIADKC': 'no label', 'WADMKK': 'no label', 'WIADKK': 'no label', 'WADMPR': 'no label', 'WIADPR': 'no label', 'TIPADM': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_KelurahanBiga_2.set('fieldLabels', {'OBJECTID': 'no label', 'NAMOBJ': 'inline label - always visible', 'FCODE': 'no label', 'REMARK': 'no label', 'METADATA': 'no label', 'SRS_ID': 'no label', 'KDBBPS': 'no label', 'KDCBPS': 'no label', 'KDCPUM': 'no label', 'KDEBPS': 'no label', 'KDEPUM': 'no label', 'KDPBPS': 'no label', 'KDPKAB': 'no label', 'KDPPUM': 'no label', 'LUASWH': 'no label', 'TIPADM': 'no label', 'WADMKC': 'no label', 'WADMKD': 'no label', 'WADMKK': 'no label', 'WADMPR': 'no label', 'WIADKC': 'no label', 'WIADKK': 'no label', 'WIADPR': 'no label', 'WIADKD': 'no label', 'UUPP': 'no label', 'LUAS': 'no label', });
+lyr_PEMUKIMAN_3.set('fieldLabels', {'NAMOBJ': 'inline label - always visible', 'FCODE': 'no label', 'REMARK': 'no label', 'SRS_ID': 'no label', 'LCODE': 'no label', 'METADATA': 'no label', 'SHAPE_Leng': 'no label', 'SHAPE_Area': 'no label', });
+lyr_PEMUKIMAN_3.on('precompose', function(evt) {
+    evt.context.globalCompositeOperation = 'normal';
+});
